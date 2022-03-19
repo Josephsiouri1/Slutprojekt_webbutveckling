bfaaff4fd811a02484b0cfc10f8797bcb474c07f
Joseph Siouri <joseph.siouri@skola.taby.se>
2022-03-19 21:39:45 +0100

    changes

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let intro_bild = document.getElementById("intro_image");
 let text_bild = document.getElementById("text_image");
-
 intro_bild.addEventListener("mouseover", function(e){
     intro_bild.style.width = "100vw";
 })
