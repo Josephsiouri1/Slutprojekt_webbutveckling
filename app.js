cfa3ce5207c21091b74d4a49c5423d355e93b7f9
Joseph Siouri <joseph.siouri@skola.taby.se>
2022-03-26 15:03:03 +0100

    changes

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 let intro_bild = document.getElementById("intro_image");
 let text_bild = document.getElementById("text_image");
-intro_bild.addEventListener("mouseover", function(e){
+
+intro_bild.addEventListener("mouseover", function(){
     intro_bild.style.width = "100vw";
 })
-intro_bild.addEventListener("mouseleave", function(e){
+intro_bild.addEventListener("mouseleave", function(){
     intro_bild.style.width = "90vw";
 })
-text_bild.addEventListener("mouseover", function(e) {
+text_bild.addEventListener("mouseover", function() {
     intro_bild.style.width = "100vw";
 })
-text_bild.addEventListener("mouseleave", function(e) {
+text_bild.addEventListener("mouseleave", function() {
     intro_bild.style.width = "90vw";
 })
 
-    
+let searchInput = document.getElementById("search-input");
+
+searchInput.onkeydown = function(event) {
+    if (event.key === "Enter") {
+        searchInput.value = ""
+    }
+}
+searchInput.onmouseleave = function() {
+        searchInput.value = ""    
+}
