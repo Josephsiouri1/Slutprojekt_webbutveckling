a9c25b1eb6917f60f3b88bc764d1556ef1c10a75
Joseph Siouri <joseph.siouri@skola.taby.se>
2022-04-02 22:45:11 +0200

    changes

---
+++ login.js
@@ -0,0 +1,48 @@
+function validationEmail() {
+    let email = document.getElementById("email").value;
+    let textError = document.getElementById("text-error");
+    let pattern =  /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if (email == "") {
+        textError.innerHTML = "";
+    }
+
+    if (email.match(pattern)) {
+        textError.innerHTML = "Your Email Adress is Valid.";
+        textError.style.color = "green";
+    }
+    else {
+        textError.innerHTML = "Please Enter Valid Email Adress";
+        textError.style.color = "red";
+    }
+
+}
+
+function validationPassword() {
+    let password = document.getElementById("password").value;
+    let textError = document.getElementById("text-error");
+    if (password == "") {
+        textError.innerHTML = "";
+    }
+    if (password.length < 6 || password.length > 20) {
+        textError.innerHTML = "Your Password must be longer than 6 and shorter the 20 characters.";
+        textError.style.color = "red";
+    }
+
+    else {
+        textError.innerHTML = "Your Password is Valid.";
+        textError.style.color = "green";
+    }
+
+}
+
+function validationSubmit() {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let textError = document.getElementById("text-error");
+    let submit = document.getElementById("submit");
+    submit.addEventListener("mousedown", function(e) {
+        if (email === "" || password === "" || textError.style.color === "red") {
+            e.preventDefault();
+        }
+    })
+}
--- product.js
+++ product.js
@@ -6,14 +6,21 @@ let greenColor = new Array("images/back_view_green.webp", "images/side_view_gree
 
 let blackColor = new Array("images/shirt_back_side.webp", "images/shirt_right_side.webp", "images/shirt_front_side.webp", "images/shirt_front_side2.webp", "images/shirt_zoom_back.webp", "images/shirt_zoom_front.webp");
 
+productColorImg[0].style.border = "solid 1px black";
+
 productColorImg[0].onmousedown = function() {
     productColor[0].innerHTML = "Black/Charcoal Grey";
+    productColorImg[0].style.border = "solid 1px black";
+    productColorImg[1].style.border = "solid 0px black";
     for (let i = 0; i < productImg.length; i++) {
         productImg[i].attributes[1].nodeValue = blackColor[i]
     }
 }
+
 productColorImg[1].onmousedown = function() {
     productColor[0].innerHTML = "Bali Green/White";
+    productColorImg[1].style.border = "solid 1px black";
+    productColorImg[0].style.border = "solid 0px black";
     for (let i = 0; i < productImg.length; i++) {
         productImg[i].attributes[1].nodeValue = greenColor[i]
     }
@@ -24,12 +31,21 @@ let numberOfProducts = document.getElementById("product-counter");
 let sizes = document.getElementsByClassName("size");
 
 for (let i = 0; i < sizes.length; i++) {
-
+    sizes[i].addEventListener("click", function(){
+        for (let j=0; j < sizes.length; j++) {
+            if (sizes[j].style.backgroundColor == "black") {
+                sizes[j].style.backgroundColor = "white"
+                sizes[j].style.color = "black"
+                sizes[i].style.backgroundColor = "black"
+                sizes[i].style.color = "white"
+            }
+        }
+        sizes[i].style.backgroundColor = "black"
+        sizes[i].style.color = "white";
+    })
+}
     
-sizes[0].addEventListener("click", function(){
-    sizes[0].style.backgroundColor = "black"
-    sizes[0].style.color = "white";
-})
+
 
     
 
@@ -44,8 +60,23 @@ function newProductNumber(currentNumber) {
     return (newNumber)
   
 }
+let errorMessage = document.createElement("p");
+
+errorMessage.innerHTML = "* Please select a size and a color to add to cart";
+errorMessage.style.color = "red";
 
 addToBag.addEventListener("click", function(e) {
     e.preventDefault();
-    numberOfProducts.innerHTML = newProductNumber(numberOfProducts.innerHTML)
-})}
+    for (let i = 0; i < sizes.length; i++) {
+        if (sizes[i].style.backgroundColor == "black") {
+            for (let j = 0; j < productColorImg.length; j++) {
+                if (productColorImg[j].onmousedown) {
+                    numberOfProducts.innerHTML = newProductNumber(numberOfProducts.innerHTML);
+                }
+            }
+        }
+    }
+    
+
+    addToBag.insertAdjacentHTML("afterend", errorMessage.innerHTML);
+})
